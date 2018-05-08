6cefed7e618ef5f5e5e5188c805c8e1d09d1555f
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-05-08 12:03:47 +0530

    added a topics list to the ui

--- src/main/python/liwon_gui.py
+++ src/main/python/liwon_gui.py
@@ -3,6 +3,8 @@ A simple UI for the liwon data
 """
 
 import wx
+import liwon
+import qindex
 
 
 class LiwonMainFrame(wx.Frame):
@@ -12,14 +14,57 @@ class LiwonMainFrame(wx.Frame):
     area at the bottom
     """
 
-    def __init__(self, title):
-        super(LiwonMainFrame, self).__init__(None, title=title,
-                                      size=(350, 250))
+    def __init__(self, qindex, title):
+        super(LiwonMainFrame, self).__init__(None,
+                                             title=title,
+                                             size=(800, 600))
+        self.qindex = qindex
+        self.topics = []
+        self.questions = []
+        self.set_state()
+
+        self.init_ui()
+        self.Center()
+
+    def init_ui(self):
+        panel = wx.Panel(self)
+
+        font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
+
+        font.SetPointSize(9)
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        st1 = wx.StaticText(panel, label='Question?')
+        st1.SetFont(font)
+        hbox1.Add(st1, flag=wx.RIGHT, border=8)
+        tc = wx.TextCtrl(panel)
+        hbox1.Add(tc, proportion=1)
+        vbox.Add(hbox1, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        topics_list_box = wx.ListBox(panel, -1, choices=self.topics)
+        hbox2.Add(topics_list_box, 1, flag=wx.EXPAND | wx.ALL, border=8)
+        vbox.Add(hbox2, 1, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, border=10)
+
+        # vbox.Add((-1, 10))
+        panel.SetSizer(vbox)
+
+    def set_state(self):
+        self.topics.extend(self.qindex.topics.keys())
 
 
 def main():
+    qidx = qindex.QIndex()
+    liwon.scan_all_files(liwon.LIWON_FOLDER + '/qna', qidx)
+    print(qidx)
+
+    print('Questions for the topic beetle are:')
+    print([x.q for x in qidx.get_qnas_for_topic('beetle')])
+
     app = wx.App()
-    ex = LiwonMainFrame(title='liwon: Learning in Wonderland')
+    ex = LiwonMainFrame(qindex=qidx, title='liwon: Learning in Wonderland')
     ex.Show()
     app.MainLoop()
 
